8f3d09c3b70e11bbf3cea4e8145fd18977f89495
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-02-24 14:01:10 +0200

    Create new Folder

---
+++ 03.Components_DeepDive/todo-list/client/src/components/TodoListItem.jsx
@@ -0,0 +1,27 @@
+import styles from './TodoListItem.module.css';
+
+/* eslint-disable react/prop-types */
+export default function TodoListItem({
+    _id,
+    text, 
+    isCompleted,
+    onStatusChange,
+}) {
+
+    const todoClassName = [styles['todo']];
+    if (isCompleted){
+        todoClassName.push(styles['is-completed'])
+    }
+  return (
+    <>
+      {/* <!-- Todo item --> */}
+      <tr className={todoClassName.join(' ')}>
+        <td>{text}</td>
+        <td>{isCompleted ? 'Complete' : 'Incomplete'}</td>
+        <td className="todo-action">
+          <button onClick={() => onStatusChange(_id)} className="btn todo-btn">Change status</button>
+        </td>
+      </tr>
+    </>
+  );
+}
