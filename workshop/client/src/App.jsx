14e5cb349a7a459c8e877c03052779092464cd46
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-04 15:40:31 +0200

    create components header, footer and user list

--- workshop/client/src/App.jsx
+++ workshop/client/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import UserList from './Components/UserList'
+
 
 function App() {
 
   return (
     <>
+      <Header />
+      <div className='main'>
+        <UserList />  
+      </div>
+      <Footer/>
     </>
   )
 }
